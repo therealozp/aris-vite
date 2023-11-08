32258ab494b6e9702c15ae69d9ab6617355b8967
therealozp <anhkhang.le0910@gmail.com>
2023-11-08 14:36:12 -0500

    integrates vite and chakra and editor todo: branch and develop theming and routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { r3f } from '@react-three/editor/vite';
-// https://vitejs.dev/config/
+
 export default defineConfig((env) => ({
 	plugins: [env.command === 'build' ? react() : r3f()],
 }));
