5b54676120dc723534afc2c2595433a34648d96c
therealozp <anhkhang.le0910@gmail.com>
2023-11-08 20:12:55 -0500

    vite is very weird, had the canvas running but r3f editor is pretty weird

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { useRef } from 'react';
+import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Sphere, Sparkles } from '@react-three/drei';
 import { Flex } from '@chakra-ui/react';
-export const App = () => (
-	<Flex h="100vh" w="100vw" alignItems="center" justifyContent={'center'}>
-		<Canvas camera={{ fov: 45, position: [-4, 2, -4] }}>
-			<Sparkles position={[1, 1, 1]} />
-			<OrbitControls makeDefault />
-			<mesh position={[6.948, -2.158, 0.465]}>
-				{/* <boxBufferGeometry attach="geometry" /> */}
-				<meshStandardMaterial attach="material" color="red" />
-			</mesh>
-			<directionalLight position={[4.224, 1.912, 3.046]} />
-			<ambientLight />
-			<Sphere position={[8.817, 1.557, 5.818]} />
-		</Canvas>
-	</Flex>
-);
+
+const App = () => {
+	return (
+		<Flex
+			h="100vh"
+			w="100vw"
+			alignItems="center"
+			justifyContent={'center'}
+			bg={'#141414'}
+		>
+			<Canvas camera={{ fov: 45, position: [-4, 2, -4] }}>
+				<gridHelper position={[0, 0, 0]} />
+				<axesHelper position={[0, 0, 0]} />
+				<Sparkles position={[1, 1, 1]} />
+				<OrbitControls makeDefault />
+				<mesh position={[6.9, -2.158, 0.465]}>
+					{/* <boxBufferGeometry attach="geometry" /> */}
+					<meshStandardMaterial
+						attach="material"
+						color={'rgb(59, 24, 24)'}
+						flatShading={true}
+					/>
+				</mesh>
+				<directionalLight position={[4.224, 1.912, 3.046]} />
+				<ambientLight />
+				<Sphere
+					position={[14.187, 6.856, 7.807]}
+					material-color={'rgb(226, 56, 56)'}
+				/>
+			</Canvas>
+		</Flex>
+	);
+};
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { r3f } from '@react-three/editor/vite';
 
+const root = resolve(__dirname, 'src');
+
 export default defineConfig((env) => ({
 	plugins: [env.command === 'build' ? react() : r3f()],
+	root,
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(root, 'index.html'),
+			},
+		},
+	},
 }));
